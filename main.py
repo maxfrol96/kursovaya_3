17209f7f90fa6d6f019769ec8aa7435dc7b4bfeb
maxfrol96 <maxfrol96@mail.ru>
2023-02-02 11:40:49 +0300

    Вторая версия

--- main.py
+++ main.py
@@ -16,25 +16,34 @@ class transaction:
         if self.trans_from == 'Данные об отправителе отсутствуют':
             card = 'Данные об отправителе отсутствуют'
         else:
-            card_number = self.trans_from[len(self.trans_from)-16:len(self.trans_from)]
-            card_name = self.trans_from.replace(card_number,'')
-            card_number = card_number[0:6] + '******' + card_number[12:16]
-            card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(4))
+            if self.trans_from[0:4] == 'Счет':
+                len_number = 20
+            else:
+                len_number = 16
+            card_number = self.trans_from[len(self.trans_from) - len_number:len(self.trans_from)]
+            card_name = self.trans_from.replace(card_number, '')
+            if len_number == 16:
+                card_number = card_number[0:6] + '******' + card_number[12:16]
+                card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(4))
+            else:
+                card_number = '****************' + card_number[16:20]
+                card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(5))
             card = card_name + card_number
         return card
 
     def blur_to(self):
-        if self.trans_desc == 'Перевод организации' or self.trans_desc == 'Открытие вклада' or self.trans_desc == 'Перевод со счета на счет':
-            card_number = self.trans_to[len(self.trans_to) - 20:len(self.trans_to)]
+        if self.trans_to[0:4] == 'Счет':
+            len_number = 20
         else:
-            card_number = self.trans_to[len(self.trans_to) - 16:len(self.trans_to)]
+            len_number = 16
+        card_number = self.trans_to[len(self.trans_to) - len_number:len(self.trans_to)]
         card_name = self.trans_to.replace(card_number, '')
-        if self.trans_desc == 'Перевод организации' or self.trans_desc == 'Открытие вклада' or self.trans_desc == 'Перевод со счета на счет':
+        if len_number == 16:
+            card_number = card_number[0:6] + '******' + card_number[12:16]
+            card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(4))
+        else:
             card_number = '****************' + card_number[16:20]
             card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(5))
-        else:
-            card_number = '************' + card_number[12:16]
-            card_number = ' '.join(card_number[i * 4:(i + 1) * 4] for i in range(4))
         card = card_name + card_number
         return card
 
@@ -64,7 +73,8 @@ for trans in transactions_list:
             last_5_trans.append(transaction(trans['date'], trans['description'], 'Данные об отправителе отсутствуют',
                                             trans['to'], trans['operationAmount']['amount'],
                                             trans['operationAmount']['currency']['name']))
-    if len(last_5_trans) == 5:
+    if len(last_5_trans) == 30:
         for trans in last_5_trans:
             print(trans.info())
         break
+
